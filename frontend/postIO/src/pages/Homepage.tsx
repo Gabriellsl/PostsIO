61f758bcefe443324694fd134a36c78057f67ab3
Gabriel Lenin <gabriellsl2009@hotmail.com>
2024-10-13 13:55:11 -0300

    feat: add project colors to tailwind

--- frontend/postIO/src/pages/Homepage.tsx
+++ frontend/postIO/src/pages/Homepage.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Homepage (){
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Posts list</h1>
+      <h1 className=''>Posts list</h1>
     </div>
   );
 }
--- frontend/postIO/tailwind.config.js
+++ frontend/postIO/tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#1A2E05',
+        'primary-foreground': '#E3E3E3',
+        'surface': '#FFFFFF',
+        'foreground': '#252525',
+        'accent': '#E5E7EB'
+      }
+    },
   },
   plugins: [],
 }
