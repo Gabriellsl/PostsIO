c06769251dec1a02a3b2034dde3f9c9eefc19f99
Gabriel Lenin <gabriellsl2009@hotmail.com>
2024-10-13 13:39:45 -0300

    feat: add tailwind css

--- frontend/postIO/src/pages/Homepage.tsx
+++ frontend/postIO/src/pages/Homepage.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Homepage (){
   return (
     <div>
-      <h1>Posts list</h1>
+      <h1 className="text-3xl font-bold underline">Posts list</h1>
     </div>
   );
 }
