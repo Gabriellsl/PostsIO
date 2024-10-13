42e2f8af7f52f82fbc6de3125cf9038fe3d629b4
Gabriel Lenin <gabriellsl2009@hotmail.com>
2024-10-13 16:24:32 -0300

    feat: create navigation component

--- frontend/postIO/src/App.tsx
+++ frontend/postIO/src/App.tsx
@@ -3,15 +3,19 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/Homepage';
 import NotFound from './pages/NotFound';
+import Navigation from './components/Navigation/Navigation';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Homepage />}></Route>
-        <Route path="*" element={<NotFound />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <>
+      <Navigation></Navigation>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ frontend/postIO/src/components/Navigation/Navigation.tsx
@@ -0,0 +1,58 @@
+import NavigationButton from './NavigationButton';
+
+function Navigation() {
+  return (
+    <div className="min-h-full">
+      <nav className="bg-primary">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="flex h-16 items-center justify-start">
+            <div className="-mr-2 flex md:hidden">
+              <button
+                type="button"
+                className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                aria-controls="mobile-menu"
+                aria-expanded="false"
+              >
+                <span className="absolute -inset-0.5"></span>
+                <span className="sr-only">Open main menu</span>
+                <svg
+                  className="block h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                  data-slot="icon"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                  />
+                </svg>
+                <svg
+                  className="hidden h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                  data-slot="icon"
+                >
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      <div className="md:hidden" id="mobile-menu">
+        <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
+          <NavigationButton title="Home" />
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Navigation;
