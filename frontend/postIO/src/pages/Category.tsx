e7bf4863fab5d85a3e69f14e5fe8be84c2b63b4b
Gabriel Lenin <gabriellsl2009@hotmail.com>
2024-10-15 17:14:07 -0300

    fix: post header favorite name

--- frontend/postIO/src/pages/Category.tsx
+++ frontend/postIO/src/pages/Category.tsx
@@ -1,14 +1,22 @@
 import { useParams } from 'react-router-dom';
 import PostList from '../components/PostList';
-import { useEffect, useState } from 'react';
+import { useCallback, useContext, useEffect, useState } from 'react';
 import { CategoryService } from '../service';
 import { Post } from '../types';
 import PostListHeader from '../components/PostList/PostListHeader';
 import PostListBody from '../components/PostList/PostListBody';
+import { CategoryContext } from '../contexts/CategortyContext';
 
 function Category() {
   const [postList, setPostList] = useState<Post[]>([]);
   const { categoryId } = useParams();
+  const categoryContext = useContext(CategoryContext);
+
+  if (!categoryContext) {
+    throw new Error('useContext must be used inside a CategoryProvider');
+  }
+
+  const { categories, loading } = categoryContext;
 
   useEffect(() => {
     if (!categoryId) return;
@@ -19,9 +27,18 @@ function Category() {
       .catch((err) => console.error(err));
   }, [categoryId]);
 
+  const handleGetCategoryName = useCallback(() => {
+    if (categories.length === 0 || loading) return '';
+
+    return categories.find((category) => category.id === categoryId)?.name;
+  }, [categoryId, categories]);
+
   return (
     <PostList>
-      <PostListHeader selectedCategory={categoryId} postsInCategory={postList.length} />
+      <PostListHeader
+        selectedCategory={handleGetCategoryName()}
+        postsInCategory={postList.length}
+      />
       <PostListBody postList={postList} selectedCategory={categoryId} />
     </PostList>
   );
